4aef9107639315d074c3e6f1ee1deb2e1da9ddd8
Harleigh Abel <harleighabel@gmail.com>
2018-01-30 02:55:05 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,10 +3,10 @@ require 'pry'
 def my_all?(array)
   if !(array.empty?)
     n = 0
-    block_return_values
+    block_return_values = []
     while n < array.length
         block_return_values << yield(array[n])
       n += 1
-    end  
+    end
   end
 end
