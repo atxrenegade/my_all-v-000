901036d138790bf4bfdb0f4e940c169216e4eb40
Harleigh Abel <harleighabel@gmail.com>
2018-01-30 03:16:22 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,11 +7,11 @@ def my_all?(array)
     while n < array.length
         block_return_values << yield(array[n])
         n += 1
+      end
+    if block_return_values.include?(false)
+      false
+    else
+      true
     end
-  end  
-  if block_return_values.include?(false)
-    false
-  else
-    true
   end
 end
